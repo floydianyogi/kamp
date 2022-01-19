8bf72f8cdcf3327a3681f221f6fc2438a881304f
Berk Cinar <63121872+floydianyogi@users.noreply.github.com>
2022-01-19 13:17:59 +0300

    Update printPrimeNumbersUpToThousand.js

--- printPrimeNumbersUpToThousand.js
+++ printPrimeNumbersUpToThousand.js
@@ -13,7 +13,7 @@ const printPrimeNumbersUpToThousand = () => {
 // Sadece 1 ve kendisine bölünebilen sayılara asal sayılar denmektedir. Asal sayılar 1 rakamı dışında başka hiçbir sayıya bölünemez. 
 // Örneğin, 3 bir asal sayıdır çünkü sadece kendisine ve 1'e tam olarak bölünebilir. Diğer bir yandan, 6 sayısı 1, 2, 3 ve 6 sayılarına tam olarak bölünebildiği için bir asal sayı değildir.
 const isPrime = (num) => {
-    for (let i = 2, s = Math.sqrt(num); i <= s; i++)
+    for (let i = 2; s = Math.sqrt(num); i <= s; i++)
       if (num % i === 0) return false;
     return num > 1;
   };
